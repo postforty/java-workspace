6b41a5d7d8b63121b809d62e76e43893beb0f88d
Dandy Code! <postforty@naver.com>
2024-07-24 16:58:51 +0900

    Update ConnectionExample.java

--- jdbc/src/ex01/ConnectionExample.java
+++ jdbc/src/ex01/ConnectionExample.java
@@ -10,12 +10,14 @@ public class ConnectionExample {
     public static void main(String[] args) {
         Connection conn = null;
         try {
+            // https://dev.mysql.com/doc/connector-j/en/connector-j-examples.html
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "1234");
 
             String sql = "SELECT * FROM city";
 
+            // https://dev.mysql.com/doc/connector-j/en/connector-j-query-attributes.html
             PreparedStatement pstmt = conn.prepareStatement(sql);
 
             ResultSet rs = pstmt.executeQuery();
